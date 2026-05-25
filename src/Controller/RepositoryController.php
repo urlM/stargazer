85a98a8bec41e740319831351c07e800272184f6
urlM <287545069+urlM@users.noreply.github.com>
2026-05-24 21:25:42 -0500

    feat(app): implement database-backed repository list view

--- src/Controller/RepositoryController.php
+++ src/Controller/RepositoryController.php
@@ -7,15 +7,15 @@ namespace App\Controller;
 use App\Repository\RepositoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class RepositoryController extends AbstractController
 {
     #[Route('/', name: 'app_repository_index', methods: ['GET'])]
-    public function __invoke(RepositoryRepository $repositories): Response
+    public function index(RepositoryRepository $repositoryRepository): Response
     {
         return $this->render('repository/index.html.twig', [
-            'repositories' => $repositories->findTopRepositories(),
+            'repositories' => $repositoryRepository->findTopRepositories(),
         ]);
     }
 }
